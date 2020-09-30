b95169b9b70673b3d434d872e0c4733efade0ddf
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-29 21:26:54 -0500

    Completed level_order_traversal_of_binary_trees.c along with changes in queue.c

--- level_order_traversal_of_binary_trees.c
+++ level_order_traversal_of_binary_trees.c
@@ -2,8 +2,46 @@
 #include "queue.c"
 
 int main() {
+  struct Node* n1 = create_node(15);
+  struct Node* n2 = create_node(11);
+  struct Node* n3 = create_node(17);
+  struct Node* n4 = create_node(5);
+  struct Node* n5 = create_node(13);
+  struct Node* n6 = create_node(16);
+  struct Node* n7 = create_node(1);
+  struct Node* n8 = create_node(12);
+
+  n1->left = n2;
+  n1->right = n3;
+
+  n2->left = n4;
+  n2->right = n5;
+
+  n3->left = n6;
+  n3->right = NULL;
+
+  n4->left = n7;
+  n4->right = NULL;
+
+  n5->left = n8;
+  n5->right = NULL;
+
+  n6->left = NULL;
+  n6->right = NULL;
+  
+  n7->left = NULL;
+  n7->right = NULL;
+
+  n8->left = NULL;
+  n8->right = NULL;
+
   struct Queue* queue = create_queue(50);
-  enqueue(queue, 5);
-  enqueue(queue, 3);
-  printQueueElements(queue);
+  enqueue(queue, n1);
+
+  while(queue->size != 0) {
+    struct Node* node = dequeue(queue);
+    if(node->left) enqueue(queue, node->left);
+    if(node->right) enqueue(queue, node->right);
+    printf("%d ", node->value);
+  }
 }
--- queue.c
+++ queue.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct Node {
+  int value;
+  struct Node* left;
+  struct Node* right;
+};
 
 struct Queue {
-  int* queue;
+  struct Node** queue;
   int front, rear, size, capacity;
 };
 
+struct Node* create_node(int);
 struct Queue* create_queue(int);
-void enqueue(struct Queue*, int);
-int dequeue();
+void enqueue(struct Queue*, struct Node*);
+struct Node* dequeue(struct Queue*);
 void printQueueElements(struct Queue*);
 
 // int main() {
@@ -30,18 +37,28 @@ void printQueueElements(struct Queue*);
 //   }
 // }
 
+struct Node* create_node(int value) {
+  // struct Node (5, 0x342342, 0x1987492)
+  // struct Node* 0x312312
+  // struct Node** [0x312312, 0x93492734]
+
+  struct Node* node = (struct Node*) malloc(sizeof(struct Node)); //0x22b2cf21
+  node->value = value; // (5, 0x2342, 0x342342) = (value, 0x2342, 0x342342)
+  return node;
+}
+
 struct Queue* create_queue(int capacity) {
-  struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+  struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
   queue->front = -1;
   queue->rear = -1;
   queue->size = 0;
   queue->capacity = capacity;
-  queue->queue = (int*) malloc(capacity * sizeof(int));
+  queue->queue = (struct Node**)malloc(capacity * sizeof(struct Node*));
 
   return queue;
 }
 
-void enqueue(struct Queue* queue, int node) {
+void enqueue(struct Queue* queue, struct Node* node) {
   if (queue->size == queue->capacity) {
     return;
   }
@@ -52,18 +69,18 @@ void enqueue(struct Queue* queue, int node) {
 
   if (queue->front == -1) {
     queue->front = 0;
-    fflush(stdout); 
+    fflush(stdout);
   }
 
   // printQueueElements(queue);
 }
 
-int dequeue(struct Queue* queue) {
+struct Node* dequeue(struct Queue* queue) {
   if (queue->size == 0) {
-    return -99999;
+    return NULL;
   }
 
-  int node = queue->queue[queue->front];
+  struct Node* node = queue->queue[queue->front];
   queue->front = (queue->front + 1) % queue->capacity;
   queue->size = queue->size - 1;
 
@@ -78,11 +95,11 @@ int dequeue(struct Queue* queue) {
 
 void printQueueElements(struct Queue* queue) {
   int counter = 0;
-  for(int i=queue->front; counter++ < queue->size; i = (i+1) % queue->capacity) {
-    printf("%d ", queue->queue[i]);
+  for (int i = queue->front; counter++ < queue->size;
+       i = (i + 1) % queue->capacity) {
+    printf("%d ", (queue->queue[i])->value);
     fflush(stdout);
   }
   printf("\n");
   fflush(stdout);
 }
-
